bb5d7e40ac6b5d5adfd4db22ca521da0452ad3cc
Ben Newell <bennewell.mail@gmail.com>
2020-11-23 21:22:21 -0600

    [add]

---
+++ foo-bar/Level 1/solution.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+num = 3
+l = [1, 2, 3, 2, 2, 2]
+
+def answer(data, n):
+    # Check if its in range
+    if len(data) in range(99):
+        print("Success! Data is in range")
+    else:
+        print("Error")
+        print("Data in list out of range. Or note that the code just quit at step.")
+    list2 = []
+    print("The number of times a number has to show up is " + str(n))
+    for m in data:
+        # Am looking for the m that shows up more than n times.
+        # This is NOT right........YEET... TODO: to remove redundanty code output. heh'; hope you understood. 
+        x = data.count(m) > n
+        print(x)
+
+answer(l, num)
